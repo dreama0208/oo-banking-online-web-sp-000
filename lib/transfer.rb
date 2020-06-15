e4b059aa277f84e05ad7ca4254a45414731f7a02
imperative-genre-4923 <dreama.lin@yale.edu>
2020-06-15 03:48:28 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,9 +28,10 @@ class Transfer
   end
 
   def reverse_transfer
-    self.sender.balance += self.amount
-    self.receiver.balance -= self.amount
-    self.status = "reversed"
+    if self.status == "complete"
+      self.sender.balance += self.amount
+      self.receiver.balance -= self.amount
+      self.status = "reversed"
   end
 
 end
