39ab5d8d324360ea50cfad7d347298d174b9f970
imperative-genre-4923 <dreama.lin@yale.edu>
2020-06-15 03:49:38 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,7 +23,7 @@ class Transfer
       self.status = "complete"
     else
       self.status = "rejected"
-      "Transaction rejected. Plese check your account balance."
+      "Transaction rejected. Please check your account balance."
     end
   end
 
