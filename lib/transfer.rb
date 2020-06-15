c6d53bfbe68bbae261a934fdf05da5f400e4071d
imperative-genre-4923 <dreama.lin@yale.edu>
2020-06-15 03:30:42 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,36 @@
 class Transfer
   # your code here
+
+  attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, status = "pending", amount)
+    @sender = sender
+    @receiver = receiver
+    @status = status
+    @amount = amount
+  end
+
+  def valid?
+    if self.sender.valid? == true && self.receiver.valid? == true
+      true
+    end
+  end
+
+  def execute_transaction
+    if self.sender.balance > self.amount
+      self.sender.balance -= self.amount
+      self.receiver.balance += self.amount
+      self.status = "complete"
+    else
+      puts "Transaction rejected. Plese check your account balance."
+      self.status = "rejected"
+    end
+  end
+
+  def reverse_transfer
+    self.sender.balance += self.amount
+    self.receiver.balance -= self.amount
+    self.status = "reversed"
+  end
+
 end
